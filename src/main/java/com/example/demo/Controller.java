b9df7d7159b5d2c030f058e61593ee58d22e1936
DarioHefti <dario.hefti@bluewin.ch>
2019-09-16 16:01:08 +0200

    name changes

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class ClientController {
+public class Controller {
 
   @Autowired
   UserRepository userRepository;
